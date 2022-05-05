3b719349ea61a41df2226b38ae4c5cfa350c9fa5
Zan <01salim.bamba@gmail.com>
2022-05-05 15:27:13 +0200

    app react, so l'étranglement arrive

--- my-app/src/services/WebService/meteo.service.js
+++ my-app/src/services/WebService/meteo.service.js
@@ -5,22 +5,15 @@ import LodashUtils from "../../ressources/utils/lodash.utils";
 export default class MeteoService {
 
     static STATIONS = [
-        {name: "", location: "FRANCE", id: "07876099999"},
-        {name: "", location: "NEW ZEALAND", id: "93420099999"},
-        {name: "", location: "CANADA", id: "71002099999"},
-        {name: "", location: "COLOMBIE", id: "80112099999"},
-        {name: "", location: "INDE", id: "43372099999"},
+        {label: "FRANCE", value: "07876099999"},
+        {label: "NEW ZEALAND", value: "93420099999"},
+        {label: "CANADA", value: "71002099999"},
+        {label: "COLOMBIE", value: "80112099999"},
+        {label: "INDE", value: "43372099999"},
     ]
 
     static YEARS = LodashUtils.range(1980, 2022);
 
 
-
-
-}
-
-class LINE {
-    label;
-    da
 
 }
