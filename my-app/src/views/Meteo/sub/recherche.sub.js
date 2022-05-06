c7e2731d8c9a41fedf6dc63e7be0d6546e6b4fbb
Zan <01salim.bamba@gmail.com>
2022-05-06 15:24:52 +0200

    J'ai amélioré la correction de Bran Stark encore une fois de plus

--- my-app/src/views/Meteo/sub/recherche.sub.js
+++ my-app/src/views/Meteo/sub/recherche.sub.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import SubTpl from "../../../components/_template/sub.tpl";
 import BlockCmn from "../../../components/_commons/block.cmn";
-import { COLORS, LINE, LINE_CHART } from "../../../ressources/utils/charts.utils";
+import {COLORS, LINE, LINE_CHART} from "../../../ressources/utils/charts.utils";
 import MeteoService from "../../../services/WebService/meteo.service";
-import { FormikControl } from "../../../ressources/utils/forms.utils";
-import { Formik, Form } from "formik";
+import {FormikControl} from "../../../ressources/utils/forms.utils";
+import {Formik, Form} from "formik";
+import {SubViewTitre} from "../../../components/atoms/view.titre";
 
-function RechercheSub(props) {
+function RechercheSub() {
 
     const [isLoaded, setIsLoaded] = useState(false)
+
+    const [filters, setFilters] = useState([])
 
     const [stations, setStations] = useState([])
     const [years, setYears] = useState([])
@@ -34,35 +37,44 @@ function RechercheSub(props) {
 
 
     const submitRecherche = async (values) => {
+        if(values.stations.length === 0) values.stations = stations.map(station => station.value)
         setIsLoaded(true)
-
+        setFilters({
+            anneMin : values.anneeMin,
+            anneMax: values.anneeMax,
+            dataSelected: MeteoService.SELECTED_DATAS.find(sd => sd.value === values.datas).label,
+            dataTypesSelected: MeteoService.SELECTED_TYPES_DATA.find(std => std.value === values.datas_types).label,
+            stations: values.stations.length
+        })
         const lines = []
 
         await Promise.all(
             values.stations.map(async (station, i) => {
                 let res = await MeteoService.getSelectedDatas(values.datas)
-                let final = res.filter(data => data.YEAR <= values.anneeMax && data.YEAR >= values.anneeMin && data.STATION === station
+                let final = res.filter(data => parseInt(data.YEAR) <= parseInt(values.anneeMax) && parseInt(data.YEAR) >= parseInt(values.anneeMin) && data.STATION === station
                 ).map(data => {
-                    return { year: parseInt(data.YEAR), value: data[values.datas_types] }
+                    return {year: parseInt(data.YEAR), value: data[values.datas_types]}
                 }).sort((a, b) => parseInt(a.year) - parseInt(b.year))
+                // console.log(final.map(obj => obj.value))
+                let line = LINE.build(
+                    stations.find(_station => _station.value === station).label,
+                    final.map(obj => obj.value),
+                    COLORS[i]
+                )
 
-                let line = LINE.build(station, final, COLORS[i])
-                lines.push({
-                    data: [...line.data.map(data => data.value)]
-                })
+               lines.push(line)
             }))
-
         setDataSets({
             datasets: lines,
-            labels: years.map(year => year.value),
+            labels: years.map(year => year.value)
+                .filter(year => parseInt(values.anneeMin) <= parseInt(year) && parseInt(year) <= parseInt(values.anneeMax)),
         })
     }
 
     const _initStationsList = () => {
         MeteoService.getSTATIONS().then(res => {
             let _stations = []
-            res.map(s => _stations.push({ label: s.NAME, value: s.STATION }))
-
+            res.map(s => _stations.push({label: s.NAME, value: s.STATION}))
             setStations(_stations)
         })
     }
@@ -72,42 +84,38 @@ function RechercheSub(props) {
             let _uniqueYears = new Set()
             let _ys = []
             res.map(s => _uniqueYears.add(s.YEAR))
-            _uniqueYears.forEach(y => _ys.push({ label: y, value: y }))
+            _uniqueYears.forEach(y => _ys.push({label: y, value: y}))
             _ys.sort((a, b) => a.label - b.label)
             setYears(_ys)
         })
     }
 
     return (
-        <SubTpl titreProps={{ titre: "recherche" }}>
+        <SubTpl titreProps={{titre: "recherche"}}>
             <div className="row row-cols-1 row-cols-lg-2 g-4">
                 <BlockCmn className="col my-3 p-3 ">
-                    <div className="card-ak border h-100 ">
-                        Ici il devrait y'avoir une map mais tu connais ligaments croisés
+                    <div className="card-ak border h-100 d-flex align-items-center justify-content-around">
+                        <p className="text-center">Ici il devrait y'avoir une map mais tu connais ligaments croisés</p>
                     </div>
                 </BlockCmn>
                 <BlockCmn className="col my-3 p-3 ">
-                    <div className=" border card-akb h-100">
+                    <div className=" border card-akb h-100 p-3">
                         <h3>Recheche de données météorologiques</h3>
-                        <hr />
+                        <hr/>
                         <Formik initialValues={initialValues} onSubmit={async (test) => await submitRecherche(test)}>
                             <Form>
                                 <div className="row row-cols-1">
-                                    <FormikControl control="select-multi" name="stations" label={"Stations"}
-                                        options={stations} />
-                                    <div className="row row-cols-2">
-                                        <FormikControl control="select" options={years} name="anneeMin"
-                                            label={"Début"} />
-                                        <FormikControl control="select" options={years} name="anneeMax" label={"Fin"} />
+                                    <FormikControl control="select-multi" name="stations" label={"Stations"} options={stations}/>
+                                    <div className="row row-cols-2 my-2">
+                                        <FormikControl control="select" options={years} name="anneeMin" label={"Début"}/>
+                                        <FormikControl control="select" options={years} name="anneeMax" label={"Fin"}/>
                                     </div>
 
-                                    <FormikControl control="select" options={MeteoService.SELECTED_DATAS} name="datas"
-                                        label={"Données"} />
-                                    <FormikControl control="select" options={MeteoService.SELECTED_TYPES_DATA}
-                                        name="datas_types" label={"Types de données annuelle"} />
+                                    <FormikControl className="my-2 "control="select" options={MeteoService.SELECTED_DATAS} name="datas" label={"Données"}/>
+                                    <FormikControl className="my-2" control="select" options={MeteoService.SELECTED_TYPES_DATA} name="datas_types" label={"Types de données annuelle"}/>
                                 </div>
 
-                                <div className="mb-3 w-100 d-flex justify-content-end">
+                                <div className="my-2 w-100 d-flex justify-content-end">
                                     <button type="submit" className="btn btn-primary">Rechercher</button>
                                 </div>
                             </Form>
@@ -119,7 +127,8 @@ function RechercheSub(props) {
             </div>
             {isLoaded && (
                 <BlockCmn className="my-3 border p-3 w-100" id="chart">
-                    <LINE_CHART data={dataSets} />
+                    <SubViewTitre className={"h6 p-5 text-center"} titre={`${filters.dataSelected} / ${filters.dataTypesSelected} entre les années ${filters.anneMin} à ${filters.anneMax} sur ${filters.stations} station(s).`}/>
+                    <LINE_CHART data={dataSets}/>
                 </BlockCmn>
             )}
 
